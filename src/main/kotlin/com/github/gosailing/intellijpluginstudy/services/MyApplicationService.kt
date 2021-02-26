98d250a80c42e59629a4d93250fc4f397c5e6927
GitHub Action <action@github.com>
2021-02-26 00:50:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/gosailing/intellijpluginstudy/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gosailing.intellijpluginstudy.services
+
+import com.github.gosailing.intellijpluginstudy.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
